c7e1a6ee7bf817309cfdbfa2f8282ee40bde6976
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-09-27 08:50:43 +0300

    Implement a websocket layer bridging C to the server

--- wsbridge.py
+++ wsbridge.py
@@ -1,6 +1,8 @@
 import ctypes
 import random
 import sys
+import json
+import websocket
 
 try:
     pujolib = ctypes.CDLL("pujolib.so")
@@ -72,12 +74,105 @@ heuristic_score = ctypes.c_double()
 h = ctypes.byref(heuristic_score)
 score = 0
 
-for _ in range(100):
-    move = pujolib.maxDropletStrategy2(g, bag, h)
-    pujolib.play_simple(g, bag, move)
-    bag.advance()
-    score += pujolib.resolve_simple(g)
-    pujolib.print_screen(s)
-    print("Score:", score)
+if False:
+    for _ in range(100):
+        move = pujolib.maxDropletStrategy2(g, bag, h)
+        pujolib.play_simple(g, bag, move)
+        bag.advance()
+        score += pujolib.resolve_simple(g)
+        pujolib.print_screen(s)
+        print("Score:", score)
 
-# TODO: Bridge to a pujo server with websocket
+
+# All possible locations and orientations right below the garbage buffer line.
+MOVES = [
+  # Orientation = 0
+  {"x1": 0, "y1": 2, "x2": 0, "y2": 1, "orientation": 0},
+  {"x1": 1, "y1": 2, "x2": 1, "y2": 1, "orientation": 0},
+  {"x1": 2, "y1": 2, "x2": 2, "y2": 1, "orientation": 0},
+  {"x1": 3, "y1": 2, "x2": 3, "y2": 1, "orientation": 0},
+  {"x1": 4, "y1": 2, "x2": 4, "y2": 1, "orientation": 0},
+  {"x1": 5, "y1": 2, "x2": 5, "y2": 1, "orientation": 0},
+  # Orientation = 1
+  {"x1": 1, "y1": 1, "x2": 0, "y2": 1, "orientation": 1},
+  {"x1": 2, "y1": 1, "x2": 1, "y2": 1, "orientation": 1},
+  {"x1": 3, "y1": 1, "x2": 2, "y2": 1, "orientation": 1},
+  {"x1": 4, "y1": 1, "x2": 3, "y2": 1, "orientation": 1},
+  {"x1": 5, "y1": 1, "x2": 4, "y2": 1, "orientation": 1},
+  # Orientation = 2
+  {"x1": 0, "y1": 1, "x2": 0, "y2": 2, "orientation": 2},
+  {"x1": 1, "y1": 1, "x2": 1, "y2": 2, "orientation": 2},
+  {"x1": 2, "y1": 1, "x2": 2, "y2": 2, "orientation": 2},
+  {"x1": 3, "y1": 1, "x2": 3, "y2": 2, "orientation": 2},
+  {"x1": 4, "y1": 1, "x2": 4, "y2": 2, "orientation": 2},
+  {"x1": 5, "y1": 1, "x2": 5, "y2": 2, "orientation": 2},
+  # Orientation = 3
+  {"x1": 0, "y1": 1, "x2": 1, "y2": 1, "orientation": 3},
+  {"x1": 1, "y1": 1, "x2": 2, "y2": 1, "orientation": 3},
+  {"x1": 2, "y1": 1, "x2": 3, "y2": 1, "orientation": 3},
+  {"x1": 3, "y1": 1, "x2": 4, "y2": 1, "orientation": 3},
+  {"x1": 4, "y1": 1, "x2": 5, "y2": 1, "orientation": 3},
+];
+
+wins = 0
+draws = 0
+losses = 0
+
+def on_message(ws, message):
+    global wins, draws, losses
+    # print("Message received", message)
+    data = json.loads(message)
+    if data["type"] == "move request":
+        ws.send(json.dumps({"type": "simple state request"}))
+    elif data["type"] == "simple state":
+        state = data["state"]
+        for j in range(NUM_PUYO_TYPES):
+            for i in range(NUM_SLICES):
+                game.screen.grid[j][i] = state["screen"]["grid"][j][i]
+        game.screen.buffered_garbage = state["screen"]["bufferedGarbage"]
+        game.pending_garbage = state["pendingGarbage"]
+        game.late_garbage = state["lateGarbage"]
+        game.move_time = state["moveTime"]
+        for i in range(COLOR_SELECTION_SIZE):
+            game.color_selection[i] = state["colorSelection"][i]
+        for i in range(len(state["bag"])):
+            bag[i] = state["bag"][i]
+
+        move = pujolib.maxDropletStrategy2(g, bag, h)
+
+        pujolib.print_screen(s)
+        print("Heuristic score:", heuristic_score.value)
+        print("W/D/L:", "{}/{}/{}".format(wins, draws, losses))
+
+        response = dict(MOVES[move])
+        response["type"] = "move"
+        ws.send(json.dumps(response))
+    elif data["type"] == "game result":
+        if data["result"] == "win":
+            wins += 1
+        elif data["result"] == "draw":
+            draws += 1
+        else:
+            losses += 1
+        print("Game over:", data["result"], data["reason"])
+        ws.send(json.dumps({"type": "game request"}))
+
+def on_error(ws, error):
+    print("Error", error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+def on_open(ws):
+    print("Connection established.")
+    ws.send(json.dumps({"type": "game request"}))
+
+if __name__ == "__main__":
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:3003",
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever()
